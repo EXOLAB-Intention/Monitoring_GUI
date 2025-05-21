7644db7a7207d17ee11292dd07af051264cae9ff
samwert452 <samwert200@gmail.com>
2025-05-21 09:16:09 +0200

    Revert "Merge branch 'main' of https://github.com/EXOLAB-Intention/Monitoring_GUI"
    This reverts commit 25c1f9aae2e54331b8f16be71fb87934675f29d4, reversing
    changes made to 69b0bad67eff8b9cfee09efab0db9856d959b5e3.

--- exo_monitoring_gui/data_generator/pmmg_generator.py
+++
@@ -1,9 +0,0 @@
-# Fichier : pmmg_generator.py
-import math
-
-class PMMGGenerator:
-    def __init__(self, num_sensors=8):
-        self.num_sensors = num_sensors
-
-    def generate(self, t):
-        return [round(110 + (10 + i) * math.sin(t + i * 0.3), 2) for i in range(self.num_sensors)]
